e5f12a86e2acb1a22585f54f8e9ab91e6a7f7d16
Hashtagich <antonchikz@mail.ru>
2023-12-14 12:24:43 +0300

    Выполнение задания 7.4.1 (HW-03). Выполнил задания из урока "D7.2. GET-параметры в действии" только paginate_by оставил со значением1, а не 10 т.к. в базе ещё нет данных (меньше 10).

--- news_portal/news/filters.py
+++ news_portal/news/filters.py
@@ -1,12 +1,29 @@
-from django_filters import FilterSet
+from django.forms import DateInput
+from django_filters import FilterSet, DateFilter, CharFilter
 from .models import Post
 
 
 class PostFilter(FilterSet):
+    datetime_post = DateFilter(
+        field_name='datetime_post',
+        widget=DateInput(attrs={'type': 'date'}),
+        lookup_expr='date__gte',
+        label='Дата'
+    )
+    title = CharFilter(
+        field_name='title',
+        lookup_expr='icontains',
+        label='Название заголовка'
+    )
+    author = CharFilter(
+        field_name='author__user__username',
+        lookup_expr='icontains',
+        label='Автор')
+
     class Meta:
         model = Post
-        fields = {
-            'datetime_post': ['lt'],
-            'title': ['icontains'],
-            'author': ['exact'],
-        }
+        fields = [
+            'title',
+            'author',
+            'datetime_post'
+        ]
